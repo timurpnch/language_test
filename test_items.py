51007e62ae2af8da9430c58bbead68b6f68eed69
timurpnch <timur.prosekin@gmail.com>
2025-02-07 02:25:44 +0300

    Changed name of func

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button(browser):
     browser.get(link)
     add_to_basket_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
 
